4b99f9a7166fea7cc5f47b0d69bff4c03dd9ef17
Jivitesh Shah <jimmyshah83@gmail.com>
2019-10-15 21:40:24 -0400

    Additional changes to trees and graphs

--- src/main/java/com/practice/coding/solutions/dataStructures/TreeNode.java
+++ src/main/java/com/practice/coding/solutions/dataStructures/TreeNode.java
@@ -1,21 +1,27 @@
 package com.practice.coding.solutions.dataStructures;
 
 /**
- * A TreeNode which could be used to implement Binary Tree or Binary Search Tree.
+ * A TreeNode which could be used to implement Binary Tree or Binary Search
+ * Tree.
  */
 public class TreeNode {
-	
-	private final Object nodeValue;
-	TreeNode left;
-	TreeNode right;
-	
-	public TreeNode(Object nodeValue) {
-		this.nodeValue = nodeValue;
-		this.left = null;
-		this.right = null;
-	}
 
-	public Object getNodeValue() {
-		return nodeValue;
-	}
+    private final Object nodeValue;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(Object nodeValue) {
+	this.nodeValue = nodeValue;
+	this.left = null;
+	this.right = null;
+    }
+
+    public Object getNodeValue() {
+	return nodeValue;
+    }
+
+    @Override
+    public String toString() {
+	return "TreeNode [nodeValue=" + nodeValue + ", left=" + left + ", right=" + right + "]";
+    }
 }
---
+++ src/main/java/com/practice/coding/solutions/stacksAndQueues/BalancedBracket.java
@@ -0,0 +1,45 @@
+package com.practice.coding.solutions.stacksAndQueues;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * Check if the bucket is balanced. I/P: {([])} O/P" True This is done using 2
+ * Stacks or ArrayDeque
+ * 
+ * @author jiviteshshah
+ *
+ */
+public class BalancedBracket {
+
+    public static String BRACKET = "{[(])}";
+    public static Stack<Character> S = new Stack<Character>();
+    
+    public static Map<Character, Character> BRACKET_PAIRS = new HashMap<Character, Character>();
+    
+    static {
+	BRACKET_PAIRS.put('(' , ')');
+	BRACKET_PAIRS.put('[' , ']');
+	BRACKET_PAIRS.put('{' , '}');
+    }
+
+    public static void main(String[] args) {
+	
+	for (char c : BRACKET.toCharArray()) {
+	    
+	    if (BRACKET_PAIRS.containsKey(c)) {
+		S.push(c);
+	    } 
+	    
+	    else {
+		char topStackVal = S.pop();
+		if (BRACKET_PAIRS.get(topStackVal) != c) {
+		    System.out.println("NO");
+		    break;
+		}
+	    }
+	}
+	
+    }
+}
